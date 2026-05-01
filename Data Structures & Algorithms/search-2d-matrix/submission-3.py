a267cd4943622909e15a830d810aa77cbe810dfe
Ethan C <169102562+EthanJClark@users.noreply.github.com>
2026-05-01 13:52:26 -0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        row_num = 0
+        for row in range(len(matrix)):
+            if matrix[row][0] >= target:
+                if matrix[row][0] == target:
+                    return True
+                else:
+                    row_num = row - 1
+                    break
+            row_num = row
+
+        print(row_num)
+        return target in matrix[row_num]
+        
